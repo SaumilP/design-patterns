00dd1833396d4009e3cd7425e45c8be8c3e03a7d
Saumil Patel <saumilpatel1@gmail.com>
2015-01-15 23:04:03 +0200

    coverity-scan.sh added

---
+++ coverity-scan.sh
@@ -0,0 +1,69 @@
+#!/bin/sh -e
+
+# Runs a Coverity scan on the local directory (which must be in a
+# DesignPatterns-scan source-tree root) and submits it.
+
+# The following variables must be set manually based on local machine directory structure:
+TOKEN=5qfopYzjggQ3AEE227Gojw
+EMAIL=saumilp@gmail.com
+COVTOOL=/Users/SaumilPatel/Tools/cov-analysis
+
+if [ ! -r paths.mk ]; then
+	echo "Cannot find 'paths.mk', run in the root of a configured DesignPatterns source tree!"
+	exit 1
+fi
+
+VERSION=`  grep '^VERSION ='   paths.mk | sed 's/^VERSION = *//'`
+PUBLISHED=`grep '^PUBLISHED =' paths.mk | sed 's/^PUBLISHED = *//'`
+
+git_dirty()
+{
+	[ "$(git status 2> /dev/null | tail -n1)" != "nothing to commit (working directory clean)" ] && echo -n '*'
+}
+git_branch()
+{
+	git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"
+}
+git_commit()
+{
+	git rev-parse HEAD | cut -c -12
+}
+
+if [ "$PUBLISHED" = release ]; then
+	DESC="release"
+elif [ -n "$PUBLISHED" ]; then
+	DESC="snapshot $PUBLISHED"
+elif [ -d .git ]; then
+	DESC="git: $(git_branch)$(git_dirty) $(git_commit)"
+else
+	DESC="unknown source on `date`"
+fi
+
+export PATH="$PATH:$COVTOOL/bin"
+
+#cov-build --dir cov-int make build build-scripts
+cov-build --dir cov-int mvn -DskipTests=true compile
+
+ARCHIVE=DesignPatterns-scan.tar.xz
+
+echo "Compressing scan directory 'cov-int' into '$ARCHIVE'..."
+
+tar caf "$ARCHIVE" cov-int
+
+echo "Submitting '$VERSION' '$DESC'"
+
+TMP=`mktemp --tmpdir curl-cov-submit-XXXXXX.html`
+
+curl --form token="$TOKEN" \
+  --form project=design-patterns
+  --form email="$EMAIL" \
+  --form file=@"$ARCHIVE" \
+  --form version="$VERSION" \
+  --form description="$VERSION - $DESC" \
+  -o $TMP https://scan.coverity.com/builds?project=SaumilP%2Fdesign-patterns
+
+cat $TMP
+
+rm -f $TMP
+
+exit 0
