cd444273d3886dc9422e3087c5e7a9e22008c892
Saumil Patel <saumilpatel1@gmail.com>
2015-01-21 00:24:57 +0200

    MVP example added

--- model-view-presenter/src/main/java/design/pattern/mvp/App.java
+++ model-view-presenter/src/main/java/design/pattern/mvp/App.java
@@ -1,12 +1,19 @@
 package design.pattern.mvp;
 
+import javax.swing.*;
+
 /**
  * Model-View-Presenter ( MVP ) Design Pattern Client
  */
 public class App {
     public static void main (String[] args){
-        View view = new View();
-        view.setPresenter(new Presenter(view, new Model()));
+        SwingUtilities.invokeLater( new Runnable() {
+            @Override public void run() {
+                View view = new View();
+                view.setPresenter(new Presenter(view, new Model()));
+            }
+        });
+
     }
 }
 
